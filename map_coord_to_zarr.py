79b91a61804b74d89c80072ffc7fc476543c4924
Loren Oh <loren.gh.oh@gmail.com>
2017-06-01 15:35:12 -0700

    Simple code mapping zyx tuple to indices

---
+++ map_coord_to_zarr.py
@@ -0,0 +1,25 @@
+import glob
+import numpy as pn
+
+# Zarr has pretty weak support outside simple slicing, so
+# we will need to load the dataset into a numpy array to 
+# begin with
+def main():
+    arr = np.random.rand(5, 5, 5)
+
+    z = np.random.randint(5, size=10)
+    y = np.random.randint(5, size=10)
+    x = np.random.randint(5, size=10)
+
+    # Just taking the values for each zyx tuple
+    ix = np.array((z, y, x))
+    print(arr[tuple(ix)])
+
+    # If you need to keep the original dimension
+    b_map = np.zeros_like(arr, dtype=bool)
+    b_map[tuple(ix)] = True
+    arr[~b_map] = 0
+    print(arr)
+
+if __name__ == '__main__':
+    main()
